2507b59b1d494bcf72a46b54650606700c5e6a45
CozyBrian <dragonbrian33@gmail.com>
2024-01-05 00:42:37 +0000

    minor change

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,24 +1,45 @@
+"use client";
 import { SykaLogo } from "@/assets/images";
+import { cn } from "@/utils";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
+const links = [
+  {
+    label: "Personal",
+    href: "/",
+  },
+  {
+    label: "Business",
+    href: "/business",
+  },
+  {
+    label: "Company",
+    href: "/pay",
+  },
+];
+
 const Header = () => {
+  const pathname = usePathname();
+
   return (
     <header className="my-5 mb-24 flex flex-row items-center justify-between max-w-[1211px] mx-auto">
       <div className="flex flex-row items-center gap-16">
         <Image src={SykaLogo} className="w-auto h-16" alt="Syka Logo" />
         <nav>
           <ul className="flex flex-row items-center gap-8 text-xenon-gray">
-            <li>
-              <Link href="/">Personal</Link>
-            </li>
-            <li>
-              <Link href="/business">Business</Link>
-            </li>
-            <li>
-              <Link href="/pay">Company</Link>
-            </li>
+            {links.map((link) => (
+              <li
+                className={cn(
+                  pathname === link.href && "text-xenon-primary font-bold",
+                )}
+                key={link.href}
+              >
+                <Link href={link.href}>{link.label}</Link>
+              </li>
+            ))}
           </ul>
         </nav>
       </div>
