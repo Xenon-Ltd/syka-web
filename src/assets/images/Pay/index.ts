37c8aebfc42294732c52b20e46c5988468a9332b
CozyBrian <dragonbrian33@gmail.com>
2024-02-12 21:06:58 +0000

    add: new skya pay

---
+++ src/assets/images/Pay/index.ts
@@ -0,0 +1,5 @@
+export { default as PayCrossBorder } from "./pay-cross-border.png";
+export { default as PayGlobe } from "./pay-globe.png";
+export { default as PayHeroDialog } from "./pay-hero-dialog.png";
+export { default as PayInvoices } from "./pay-invoices.png";
+export { default as PayTransactions } from "./pay-transactions.png";
