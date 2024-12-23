b10338127bff9b001a87d6271a0b6bd1e5d03a62
CozyBrian <dragonbrian33@gmail.com>
2024-12-23 17:44:09 +0000

    countries

--- src/components/pay/countries.tsx
+++ src/components/pay/countries.tsx
@@ -6,69 +6,136 @@ import React from "react";
 const countries = [
   {
     name: "Ghana",
-    Icon: COUNTRIES_ICON.GhanaIcon,
+    Icon: COUNTRIES_ICON.GH,
     pending: false,
   },
   {
-    name: "Nigeria",
-    Icon: COUNTRIES_ICON.NigeriaIcon,
+    name: "United Arab Emirates (AED) ",
+    Icon: COUNTRIES_ICON.AE,
     pending: false,
   },
   {
-    name: "Chad",
-    Icon: COUNTRIES_ICON.ChadIcon,
+    name: "Argentina (ARS)",
+    Icon: COUNTRIES_ICON.AR,
     pending: false,
   },
   {
-    name: "Equatorial Guinea",
-    Icon: COUNTRIES_ICON.GuineaIcon,
+    name: "Brazil (BRL) ",
+    Icon: COUNTRIES_ICON.BR,
     pending: false,
   },
   {
-    name: "Gabon",
-    Icon: COUNTRIES_ICON.GabonIcon,
+    name: "Chile (CLP)",
+    Icon: COUNTRIES_ICON.CL,
     pending: false,
   },
   {
-    name: "Congo",
-    Icon: COUNTRIES_ICON.CongoIcon,
+    name: "Eurozone countries (EUR) ",
+    Icon: COUNTRIES_ICON.EURO,
     pending: false,
   },
   {
-    name: "Benin",
-    Icon: COUNTRIES_ICON.BeninIcon,
-    pending: true,
+    name: "United Kingdom (GBP)",
+    Icon: COUNTRIES_ICON.GB,
+    pending: false,
   },
   {
-    name: "Togo",
-    Icon: COUNTRIES_ICON.TogoIcon,
-    pending: true,
+    name: "Indonesia (IDR)",
+    Icon: COUNTRIES_ICON.ID,
+    pending: false,
   },
+  {
+    name: "India (INR)",
+    Icon: COUNTRIES_ICON.IN,
+    pending: false,
+  },
+  {
+    name: "Mexico (MXN)",
+    Icon: COUNTRIES_ICON.MX,
+    pending: false,
+  },
+  {
+    name: "Nigeria (NGN)",
+    Icon: COUNTRIES_ICON.NG,
+    pending: false,
+  },
+  {
+    name: "Peru (PEN)",
+    Icon: COUNTRIES_ICON.PE,
+    pending: false,
+  },
+  {
+    name: "Philippines (PHP)",
+    Icon: COUNTRIES_ICON.PH,
+    pending: false,
+  },
+  {
+    name: "Turkey (TRY)",
+    Icon: COUNTRIES_ICON.TH,
+    pending: false,
+  },
+  {
+    name: "Turkey (TRY)",
+    Icon: COUNTRIES_ICON.TR,
+    pending: false,
+  },
+  {
+    name: "Vietnam (VND)",
+    Icon: COUNTRIES_ICON.VN,
+    pending: false,
+  },
+  // {
+  //   name: "Chad",
+  //   Icon: COUNTRIES_ICON.ChadIcon,
+  //   pending: false,
+  // },
+  // {
+  //   name: "Equatorial Guinea",
+  //   Icon: COUNTRIES_ICON.GuineaIcon,
+  //   pending: false,
+  // },
+  // {
+  //   name: "Gabon",
+  //   Icon: COUNTRIES_ICON.GabonIcon,
+  //   pending: false,
+  // },
+  // {
+  //   name: "Congo",
+  //   Icon: COUNTRIES_ICON.CongoIcon,
+  //   pending: false,
+  // },
+  // {
+  //   name: "Benin",
+  //   Icon: COUNTRIES_ICON.BeninIcon,
+  //   pending: true,
+  // },
+  // {
+  //   name: "Togo",
+  //   Icon: COUNTRIES_ICON.TogoIcon,
+  //   pending: true,
+  // },
 ];
 
 const CountriesWeSupport = () => {
   return (
-    <section className="xl:w-[1211px] mx-auto flex flex-col gap-12 items-center">
+    <section className="xl:w-[1240px] mx-auto flex flex-col gap-12 items-center">
       <h2 className="font-dmSans font-bold text-lg xl:text-4.5xl text-xenon-gray">
         Countries We Currently Support
       </h2>
-      <div className="flex flex-col xl:flex-row xl:flex-wrap items-stretch xl:items-center justify-center gap-16">
+      <div className="flex flex-col xl:flex-row xl:flex-wrap items-stretch xl:items-center justify-center gap-5 ">
         {countries.map((country, index) => (
           <div
             key={index}
-            className="flex flex-row gap-3 items-center bg-gray-100 rounded-full py-2 px-6"
+            className="flex flex-row gap-3 items-center bg-gray-100 rounded-full py-2 px-4"
           >
-            <div className="w-6 h-6 xl:w-10 xl:h-10 rounded-full overflow-clip bg-gray-300">
+            <div className="w-6 h-6 rounded-full overflow-clip bg-gray-300">
               <Image
                 src={country.Icon}
                 alt="country-icon"
                 className="w-full h-full"
               />
             </div>
-            <p className="text-base xl:text-2xl">{country.name}</p>
-            {index >= 6 && (
-              <Image src={HourGlass03} alt="country-icon" className="w-4 h-4" />
-            )}
+            <p className="text-base">{country.name}</p> 
           </div>
         ))}
       </div>
